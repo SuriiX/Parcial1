b8eaf9e203e0bd653b05c894a5cf70e897a62b53
Admin <Admin@DESKTOP-LNAE4JP>
2025-03-12 21:12:33 -0500

    Add project files.

---
+++ Clases/clsOpeComputador.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using Parcial1.Models;
+
+namespace Parcial1.Clases
+{
+    public class clsOpeComputador
+    {
+        private ITMEntities db = new ITMEntities();
+        public Computador computador { get; set; }
+
+        public string Insertar()
+        {
+            try
+            {
+                db.Computadors.Add(computador);
+                db.SaveChanges();
+
+                return $"✅ Computador insertado correctamente. ID: {computador.ComputadorID}";
+            }
+            catch (Exception ex)
+            {
+                return $"❌ Error al insertar el computador: {ex.Message}";
+            }
+        }
+
+        public Computador Consultar(int id)
+        {
+            return db.Computadors.FirstOrDefault(c => c.ComputadorID == id);
+        }
+
+        public string Actualizar()
+        {
+            try
+            {
+                Computador compu = Consultar(computador.ComputadorID);
+                if (compu == null)
+                {
+                    return "⚠️ No se encontró el computador con ese ID";
+                }
+
+                db.Computadors.AddOrUpdate(computador);
+                db.SaveChanges();
+
+                return $"✅ Computador actualizado correctamente. ID: {computador.ComputadorID}";
+            }
+            catch (Exception ex)
+            {
+                return $"❌ No se pudo actualizar el computador: {ex.Message}";
+            }
+        }
+
+        public List<Computador> ConsultarTodos()
+        {
+            return db.Computadors.OrderBy(c => c.MarcaProcesador).ToList();
+        }
+
+        public string Eliminar(int id)
+        {
+            try
+            {
+                Computador compu = Consultar(id);
+                if (compu == null)
+                {
+                    return "⚠️ No se encontró el computador con ese ID";
+                }
+
+                db.Computadors.Remove(compu);
+                db.SaveChanges();
+
+                return $"✅ Computador eliminado correctamente. ID: {id}";
+            }
+            catch (Exception ex)
+            {
+                return $"❌ No se pudo eliminar el computador: {ex.Message}";
+            }
+        }
+    }
+}
---
+++ Controllers/ComputadorController.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Parcial1.Models;
+using Parcial1.Clases;
+namespace apiCineClub.Controllers
+{
+    [RoutePrefix("api/Computadores")]
+    public class ComputadoresController : ApiController
+    {
+        [HttpGet]
+        [Route("ConsultarTodos")]
+        public List<Computador> ConsultarTodos()
+        {
+            clsOpeComputador computador = new clsOpeComputador();
+            return computador.ConsultarTodos();
+        }
+
+        [HttpGet]
+        [Route("ConsultarXID")]
+        public Computador ConsultarXID(int id)
+        {
+            clsOpeComputador computador = new clsOpeComputador();
+            return computador.Consultar(id);
+        }
+
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] Computador computador)
+        {
+            clsOpeComputador opeComputador = new clsOpeComputador();
+            opeComputador.computador = computador;
+            return opeComputador.Insertar();
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] Computador computador)
+        {
+            clsOpeComputador opeComputador = new clsOpeComputador();
+            opeComputador.computador = computador;
+            return opeComputador.Actualizar();
+        }
+
+        [HttpDelete]
+        [Route("EliminarXID")]
+        public string EliminarXID(int id)
+        {
+            clsOpeComputador opeComputador = new clsOpeComputador();
+            return opeComputador.Eliminar(id);
+        }
+    }
+}
